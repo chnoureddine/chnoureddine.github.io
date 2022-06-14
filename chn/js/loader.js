868d10f9502210a6e3f26817f95817462eca7d31
chnoureddine <nchekker@gmail.com>
2022-06-14 23:48:06 +0100

    maj

--- chn/js/loader.js
+++ chn/js/loader.js
@@ -1,10 +1,11 @@
 function loader() {
     document.querySelector(".loader").style.display = "none";
     document.querySelector(".container").style.display = "block";
+    document.querySelector(".footer").style.display = "block";
 }
 
 function fadeOut() {
-    setInterval(loader, 4000);
+    setInterval(loader, 2000);
 }
 
 window.onload = fadeOut;
--- chn/js/navbar.js
+++ chn/js/navbar.js
@@ -7,3 +7,13 @@ document.querySelector("#navbar").onclick = function() {
         navbar.style.display = "none";
     }
 };
+
+let navbarpromos = document.querySelector(".headerpromos .links ul");
+
+document.querySelector("#navbarpromos").onclick = function() {
+    if (navbarpromos.style.display === "none") {
+        navbarpromos.style.display = "block";
+    } else {
+        navbarpromos.style.display = "none";
+    }
+};
