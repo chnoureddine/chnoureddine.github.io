70bd4d487b984b5b2048bf0dd62f46f42eb0e868
chnoureddine <nchekker@gmail.com>
2022-06-18 00:59:34 +0100

    maj

--- chn/js/loader.js
+++ chn/js/loader.js
@@ -1,7 +1,7 @@
 function loader() {
     document.querySelector(".loader").style.display = "none";
     document.querySelector(".container").style.display = "block";
-    document.querySelector(".footer").style.display = "block";
+    document.querySelector(".footer").style.display = "flex";
 }
 
 function fadeOut() {
