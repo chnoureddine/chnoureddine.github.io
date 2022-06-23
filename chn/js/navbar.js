7f017b9e69c0e6f6df52a85b88f2488ce6932f6c
chnoureddine <nchekker@gmail.com>
2022-06-23 15:21:59 +0100

    maj

--- chn/js/navbar.js
+++ chn/js/navbar.js
@@ -2,7 +2,7 @@ let navbar = document.querySelector(".header .links ul");
 
 document.querySelector("#navbar").onclick = function() {
     if (navbar.style.display === "none") {
-        navbar.style.display = "block";
+        navbar.style.display = "inline-block";
     } else {
         navbar.style.display = "none";
     }
