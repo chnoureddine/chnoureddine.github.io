9dc440a17961267eab7b11c48c8bc7b58c7c1478
chnoureddine <nchekker@gmail.com>
2023-12-09 00:45:01 +0100

    Add files via upload

---
+++ js/navbar.js
@@ -0,0 +1,19 @@
+let navbar = document.querySelector(".header .links ul");
+
+document.querySelector("#navbar").onclick = function() {
+    if (navbar.style.display === "none") {
+        navbar.style.display = "inline-block";
+    } else {
+        navbar.style.display = "none";
+    }
+};
+
+// let navbarpromos = document.querySelector("#headerpromos .links ul");
+
+// document.querySelector("#navbarpromos").onclick = function() {
+//     if (navbarpromos.style.display === "none") {
+//         navbarpromos.style.display = "block";
+//     } else {
+//         navbarpromos.style.display = "none";
+//     }
+// };
